112b6d17a3e0370b8ff0df8786cb378213fab5a4
AbdelrahmanTaha <74047668+bedotaha1@users.noreply.github.com>
2024-12-14 03:33:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,188 @@
+import { useState } from 'react'
+import about from '../public/images/about.jpeg'
+import favicon from '../public/images/favicon.ico'
+import logo from '../public/images/logo.svg'
+import main from '../public/images/main.jpeg'
+
+import { tourCards, Services, lorem, links, footerIcons } from './Data/data.js'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const nav = ['home', 'about', 'services', 'tours']
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="backroads" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <ul className="nav-links" id="nav-links">
+            {nav.map((key, index) => {
+              return (
+                <li key={index}>
+                  <a href={`#${key}`} className="nav-link">
+                    {key}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+
+          <ul className="nav-icons">
+            {footerIcons.map((footericon, index) => {
+              const { title, link } = footericon
+              return (
+                <li key={index}>
+                  <a href={link} target="_blank" className="nav-icon">
+                    <i className={`fab fa-${title}`}></i>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </nav>
+
+      <section className="hero" id="home">
+        <div className="hero-banner">
+          <h1>continue exploring</h1>
+          <p>{lorem}</p>
+          <a href="#tours" className="btn hero-btn">
+            explore tours
+          </a>
+        </div>
+      </section>
+
+      <section className="section" id="about">
+        <div className="section-title">
+          <h2>
+            about <span>us</span>
+          </h2>
+        </div>
+
+        <div className="section-center about-center">
+          <div className="about-img">
+            <img src={main} className="about-photo" alt="awesome beach" />
+          </div>
+          <article className="about-info">
+            <h3>explore the difference</h3>
+            <p>{lorem}</p>
+            <p>{lorem}</p>
+            <a href="#" className="btn">
+              read more
+            </a>
+          </article>
+        </div>
+      </section>
+
+      <section className="section services" id="services">
+        <div className="section-title">
+          <h2>
+            our <span>services</span>
+          </h2>
+        </div>
+        <div className="section-center services-center">
+          {Services.map((service, index) => {
+            const { heading, description } = service
+            return (
+              <article className="service" key={index}>
+                <span className="service-icon">
+                  <i className="fas fa-wallet fa-fw"></i>
+                </span>
+                <div className="service-info">
+                  <h4 className="service-title">{heading}</h4>
+                  <p className="service-text">{description}</p>
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </section>
+
+      <section className="section" id="tours">
+        <div className="section-title">
+          <h2>
+            featured <span>tours</span>
+          </h2>
+        </div>
+
+        <div className="section-center featured-center">
+          {tourCards.map((tourcard, index) => {
+            const {
+              imgSrc,
+              tourDate,
+              tourTitle,
+              tourTime,
+              tourDescription,
+              tourDistination,
+              tourCost,
+            } = tourcard
+            return (
+              <article className="tour-card" key={index}>
+                <div className="tour-img-container">
+                  <img src={imgSrc} className="tour-img" alt="" />
+                  <p className="tour-date">{tourDate}</p>
+                </div>
+                <div className="tour-info">
+                  <div className="tour-title">
+                    <h4>{tourTitle}</h4>
+                  </div>
+                  <p>{tourDescription}</p>
+                  <div className="tour-footer">
+                    <p>
+                      <span>
+                        <i className="fas fa-map"></i>
+                      </span>
+                      {tourDistination}
+                    </p>
+                    <p>{tourTime}</p>
+                    <p>{tourCost}</p>
+                  </div>
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </section>
+
+      <footer className="section footer">
+        <ul className="footer-links">
+          {links.map((obj, index) => {
+            const { title, link } = obj
+            return (
+              <li key={index}>
+                <a href={link} className="footer-link">
+                  {title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <ul className="footer-icons">
+          {footerIcons.map((footericon, index) => {
+            const { link, title } = footericon
+            return (
+              <li key={index}>
+                <a href={link} target="_blank" className="footer-icon">
+                  <i className={`fab fa-${title}`}></i>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+        <p className="copyright">
+          copyright &copy; Backroads travel tours company
+          <span id="date"></span> all rights reserved
+        </p>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/Data/data.js
@@ -0,0 +1,115 @@
+export const tourCards = [
+    {
+        imgSrc: './images/tour-1.jpeg',
+        tourDate: "August 26th, 2020",
+        tourTitle: "Tibet Adventure",
+        tourDescription:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        tourDistination: "China",
+        tourTime: "6 Days",
+        tourCost: "From $2100",
+    },
+    {
+        imgSrc: "./images/tour-2.jpeg",
+        tourDate: "October 1st, 2020",
+        tourTitle: "Best of Java",
+        tourDescription:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        tourDistination: "Indonesia",
+        tourTime: "11 Days",
+        tourCost: "From $1400",
+    },
+    {
+        imgSrc: "./images/tour-3.jpeg",
+        tourDate: "September 15th, 2020",
+        tourTitle: "Explore Hong Kong",
+        tourDescription:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        tourDistination: "Hong Kong",
+        tourTime: "8 Days",
+        tourCost: "From $5000",
+    },
+    {
+        imgSrc: "./images/tour-4.jpeg",
+        tourDate: "December 5th, 2019",
+        tourTitle: "Kenya Highlights",
+        tourDescription:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        tourDistination: "Kenya",
+        tourTime: "20 Days",
+        tourCost: "From $3300",
+    },
+    {
+        imgSrc: "./images/tour-5.jpeg",
+        tourDate: "December 6th, 2017",
+        tourTitle: "Zoo",
+        tourDescription:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        tourDistination: "South Africa",
+        tourTime: "9 Days",
+        tourCost: "From $3500",
+    },
+    {
+        imgSrc: "./images/tour-6.jpeg",
+        tourDate: "may 4th, 2021",
+        tourTitle: "The Taj Mahal",
+        tourDescription:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        tourDistination: "India",
+        tourTime: "30 Days",
+        tourCost: "From $3600",
+    },
+];
+
+
+export const Services = [
+    {
+        heading: "Saving Money",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit, Asperiores, officia"
+    },
+    {
+        heading: "Endless Hiking",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit, Asperiores, officia"
+    },
+    {
+        heading: "Amazing Comfort",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit, Asperiores, officia"
+    }
+]
+
+export const lorem = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur quisquam harum nam cumque temporibus explicabo dolorumsapiente odio unde dolor ?"
+
+
+
+export const links = [
+    {
+        title: "Home",
+        link: "#home"
+    },
+    {
+        title: "about",
+        link: "#about"
+    },
+    {
+        title: "Services",
+        link: "#services"
+    }, {
+        title: "featured",
+        link: "#featured"
+    },
+]
+
+export const footerIcons = [
+    {
+        title: "facebook",
+        link: "https://www.facebook.com"
+    },
+    {
+        title: "twitter",
+        link: "https://www.twitter.com"
+    },
+    {
+        title: "squarespace",
+        link: "https://www.squarespace.com"
+    }
+]
